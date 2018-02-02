98121dd475525f5763ce0a1cededc82f53ddefef
Wippa <wippa@Wippa-MBP2.local>
2018-02-03 03:41:33 +0700

    exercise 7

---
+++ exercise-7.js
@@ -0,0 +1,38 @@
+Soal no.1
+var rows1 = 0
+var limit = 5
+while (rows1 < limit) {
+  console.log('*') ;
+  rows1 ++ ;
+}
+
+
+Soal No.2
+var rows2 = 0 ;
+var limit = 5 ;
+while (rows2 < limit) {
+  var limitin = 0 ;
+  var star = '';
+  while (limitin < limit ){
+    star += '*';
+    limitin ++ ;
+  }
+  console.log(star) ;
+  rows2 ++ ;
+}
+
+
+Soal No.3
+
+var rows2 = 0 ;
+var limit = 5 ;
+while (rows2 < limit) {
+  var limitin = 0 ;
+  var star = '';
+  while (limitin <= rows2 ){
+    star += '*';
+    limitin ++ ;
+  }
+  console.log(star) ;
+  rows2 ++ ;
+}
