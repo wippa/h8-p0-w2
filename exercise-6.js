2fd7a54a63b10a7e8432512bf6d07a001dacccce
Wippa <wippa@Wippa-MBP2.local>
2018-02-03 02:33:23 +0700

    exercise 6

---
+++ exercise-6.js
@@ -0,0 +1,31 @@
+Soal No.1
+var index = 2;
+var inc = 2 ;
+console.log('LOOPING PERTAMA') ;
+while(index <= 20) {
+  console.log(index +' '+ '- I love coding');
+  index += inc;
+}
+index -=2 ;
+console.log('LOOPING KEDUA') ;
+while(index >= 2) {
+  console.log(index +' '+ '- I will become fullstack developer ');
+  index -= inc;
+}
+
+
+Soal No.2
+console.log('LOOPING PERTAMA') ;
+for(var deret = 1; deret <= 20; deret ++) {
+  console.log(deret + ' '+ '- I love coding');
+}
+
+console.log('LOOPING KEDUA') ;
+
+for(var deret = 20; deret > 0; deret --) {
+  console.log(deret + '' + '- I will become fullstack developer');
+}
+
+
+Soal No.3
+.................
