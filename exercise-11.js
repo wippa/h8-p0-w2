9e398b9a898aba144f55e14583f9768f3f489e7f
Wippa <wippa@Wippa-MBP2.local>
2018-02-04 04:28:51 +0700

    exercise 11

---
+++ exercise-11.js
@@ -0,0 +1,17 @@
+function balikKata(str) {
+    var split = str.split("");
+
+
+    var reverse = split.reverse();
+
+
+    var join = reverse.join("");
+    return join;
+}
+
+
+console.log(balikKata('Hello World and Coders')); // sredoC dna dlroW olleH
+console.log(balikKata('John Doe')); // eoD nhoJ
+console.log(balikKata('I am a bookworm')); // mrowkoob a ma I
+console.log(balikKata('Coding is my hobby')); // ybboh ym si gnidoC
+console.log(balikKata('Super')); // repuS
